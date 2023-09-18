985b31e87492fc18dcac323c8c450c1fd7a65718
Prajwal Choudhary <prajwalchoudhary14@gmail.com>
2023-09-18 21:11:07 +0530

    learned about using TS inside JS classes

--- Lesson-1/build/js/main.js
+++ Lesson-1/build/js/main.js
@@ -1,33 +1,114 @@
 "use strict";
-// Type assertion or casting
-// Convert to more or less specific type
-let a = 'Hello';
-let b = a; // less specific
-// Hover over this by setting type One = number
-// let c = a as Three // more specific
-let c = a;
-let d = 'world';
-let e = 'world';
-// a use case for type assertion
-const addOrConcat = (a, b, c) => {
-    if (c === 'add') {
-        return a + b;
+// This seems quite redundant but we can do better with data access modifiers.
+// class Coder {
+//     // property needs to exist in the class and same 
+//     // needs to be provided inside the constructor.
+//     name: string
+//     music: string
+//     age: number
+//     lang: string
+//     constructor(name: string, music: string, age: number, lang: string) {
+//         this.name = name;
+//         this.music = music;
+//         this.age = age;
+//         this.lang = lang;
+//     }
+// }
+// Let's no repeat ourselves
+class Coder {
+    constructor(name, music, 
+    // can only be accessed inside this class (what that means?)
+    age, 
+    // like private you can't access this outside class
+    // but unlike private protected data member can be
+    // accessed inside derived class.
+    lang = 'Typescript') {
+        this.name = name;
+        this.music = music;
+        this.age = age;
+        this.lang = lang;
+        this.name = name;
+        this.music = music;
+        this.age = age;
+        this.lang = lang;
     }
-    return '' + a + b;
-};
-// essentially we are telling react we know better
-let myVal = addOrConcat(2, 2, 'concat');
-// be careful -  a string is returned
-let nextVal = addOrConcat(2, 2, 'concat');
-let currVal = addOrConcat(2, 2, 'concat');
-// forced type conversion
-10;
-// The DOM this is where type assertion really shines
-// ! -> non-null assertion
-// we are telling typescript that we know better than it and 
-// their is an image by id '#img' as it is not going to be null
-const myImg = document.getElementById('#img');
-const img = document.querySelector('img');
-const nextImg = document.querySelector('img');
-img.src;
-myImg.src;
+    getAge() {
+        return `Hello, I'm ${this.age} years old.`;
+    }
+}
+const Prajwal = new Coder('Prajwal', 'Rock', 23);
+console.log(Prajwal.getAge());
+// console.log(Prajwal.age);
+// console.log(Prajwal.lang);
+class WebDev extends Coder {
+    constructor(computer, name, age, music) {
+        super(name, music, age);
+        this.computer = computer;
+        this.computer = computer;
+    }
+    getLang() {
+        return `I write in ${this.lang}`;
+    }
+}
+const Mike = new WebDev('Dell Inspiron', 'Mike', 28, 'Hip-hop');
+console.log(Mike.getLang());
+class Guitarist {
+    constructor(name, instrument) {
+        this.name = name;
+        this.instrument = instrument;
+    }
+    play(action) {
+        return `${this.name} ${action} the ${this.instrument}`;
+    }
+}
+const Page = new Guitarist('Jimmy', 'guitar');
+console.log(Page.play('strums'));
+///////////////////////////////////////////////////////////////////
+class Peeps {
+    static getCount() {
+        return Peeps.count;
+    }
+    constructor(name, id = 0) {
+        this.name = name;
+        this.id = id;
+        this.name = name;
+        this.id = ++Peeps.count;
+    }
+}
+// above we have seen data members being initialized inside constructor
+// and each of those members is associated with an instance of the class.
+// But data members and functions declared with keyword static in-front
+// of them are associated directly to the class instead of being unique
+// for each instance of class we create. (Are these members and functions 
+// global for all instance of the class we create?)
+Peeps.count = 0;
+const Steve = new Peeps('Steve');
+const John = new Peeps('John');
+const Amy = new Peeps('Amy');
+console.log(Steve.id);
+console.log(John.id);
+console.log(Amy.id);
+console.log(Peeps.count);
+///////////////////////////////////////////////////////////////
+// Getters and setters
+class Bands {
+    constructor() {
+        this.dataState = [];
+    }
+    get data() {
+        return this.dataState;
+    }
+    set data(value) {
+        if (Array.isArray(value) && value.every(el => typeof el === 'string')) {
+            this.dataState = value;
+        }
+        else {
+            throw new Error('Param is not an array of strings');
+        }
+    }
+}
+const MyBands = new Bands();
+MyBands.data = ['Neil Young', 'Jimmy Hendrix'];
+console.log(MyBands.data);
+MyBands.data = [...MyBands.data, 'Led Zep', 'Pink Floyd'];
+console.log(MyBands.data);
--- Lesson-1/src/main.ts
+++ Lesson-1/src/main.ts
@@ -1,47 +1,160 @@
-// Type assertion or casting
+// This seems quite redundant but we can do better with data access modifiers.
+// class Coder {
+//     // property needs to exist in the class and same 
+//     // needs to be provided inside the constructor.
+//     name: string
+//     music: string
+//     age: number
+//     lang: string
 
-type One = string;
-type Two = string | number;
-type Three = 'Hello'
+//     constructor(name: string, music: string, age: number, lang: string) {
+//         this.name = name;
+//         this.music = music;
+//         this.age = age;
+//         this.lang = lang;
+//     }
+// }
 
-// Convert to more or less specific type
-let a: One = 'Hello'
-let b = a as Two // less specific
+// Let's no repeat ourselves
+class Coder {
+    // ! is an assertion and don't use it too much
+    // with this assertion initializing secondLang
+    // in constructor in not necessary 
+    secondLang!: string
 
-// Hover over this by setting type One = number
-// let c = a as Three // more specific
+    constructor(
+        public readonly name: string,
+        public music: string,
+        // can only be accessed inside this class (what that means?)
+        private age: number,
+        // like private you can't access this outside class
+        // but unlike private protected data member can be
+        // accessed inside derived class.
+        protected lang: string = 'Typescript'
+    ) {
+        this.name = name;
+        this.music = music;
+        this.age = age;
+        this.lang = lang;
+    }
 
-let c = a as Three 
-
-let d = <One>'world'
-let e = <string | number>'world'
-
-// a use case for type assertion
-const addOrConcat = (a: number, b: number, c: ('add' | 'concat')): (number | string) => {
-    if(c === 'add') {
-        return a + b;
-    } 
-    return '' + a + b;
+    public getAge() {
+        return `Hello, I'm ${this.age} years old.`
+    }
 }
 
-// essentially we are telling react we know better
-let myVal: string = addOrConcat(2, 2, 'concat') as string;
+const Prajwal =  new Coder('Prajwal', 'Rock', 23);
+console.log(Prajwal.getAge());
+// console.log(Prajwal.age);
+// console.log(Prajwal.lang);
 
-// be careful -  a string is returned
-let nextVal: number = addOrConcat(2, 2, 'concat') as number;
-let currVal: Three = addOrConcat(2, 2, 'concat') as Three;
+class WebDev extends Coder {
+    constructor(
+        public computer: string,
+        name: string,
+        age: number,
+        music: string
+    ) {
+        super(name, music, age);
+        this.computer = computer;
+    }
 
-// forced type conversion
-(10 as unknown) as string
+    public getLang() {
+        return `I write in ${this.lang}`
+    }
+}
 
-// The DOM this is where type assertion really shines
+const Mike = new WebDev('Dell Inspiron', 'Mike', 28, 'Hip-hop');
+console.log(Mike.getLang());
+// console.log(Mike.age);
+// console.log(Mike.lang);
 
-// ! -> non-null assertion
-// we are telling typescript that we know better than it and 
-// their is an image by id '#img' as it is not going to be null
-const myImg = document.getElementById('#img')! as HTMLImageElement;
-const img = document.querySelector('img') as HTMLImageElement;
-const nextImg = <HTMLImageElement> document.querySelector('img');
+//////////////////////////////////////////////////////////////////
 
-img.src
-myImg.src
+// Class implementing an interface.
+interface Musician {
+    name: string,
+    instrument: string,
+    play(action: string): string
+}
+
+class Guitarist implements Musician{
+    name: string
+    instrument: string
+
+    constructor(name: string, instrument: string) {
+        this.name = name;
+        this.instrument = instrument;
+    }
+
+    play(action: string): string {
+        return `${this.name} ${action} the ${this.instrument}`
+    }
+}
+
+const Page = new Guitarist('Jimmy', 'guitar')
+console.log(Page.play('strums'));
+
+///////////////////////////////////////////////////////////////////
+
+class Peeps {
+    // above we have seen data members being initialized inside constructor
+    // and each of those members is associated with an instance of the class.
+    // But data members and functions declared with keyword static in-front
+    // of them are associated directly to the class instead of being unique
+    // for each instance of class we create. (Are these members and functions 
+    // global for all instance of the class we create?)
+    static count:number = 0
+
+    static getCount():number {
+        return Peeps.count;
+    }
+
+    constructor(
+        public name: string,
+        public id: number = 0,
+    ) {
+        this.name = name;
+        this.id = ++Peeps.count;
+    }
+
+
+}
+
+const Steve = new Peeps('Steve');
+const John = new Peeps('John');
+const Amy = new Peeps('Amy');
+
+console.log(Steve.id);
+console.log(John.id);
+console.log(Amy.id);
+
+console.log(Peeps.count);
+///////////////////////////////////////////////////////////////
+
+// Getters and setters
+class Bands {
+    private dataState: string[]
+
+    constructor() {
+        this.dataState = []
+    }
+
+    public get data(): string[] {
+        return this.dataState
+    }
+
+    public set data(value: string[]) {
+        if(Array.isArray(value) && value.every(el => typeof el === 'string')) {
+            this.dataState = value
+        } else {
+            throw new Error('Param is not an array of strings')
+        }
+    } 
+}
+
+const MyBands = new Bands();
+MyBands.data = ['Neil Young', 'Jimmy Hendrix'];
+console.log(MyBands.data);
+MyBands.data = [...MyBands.data, 'Led Zep', 'Pink Floyd'];
+console.log(MyBands.data);
