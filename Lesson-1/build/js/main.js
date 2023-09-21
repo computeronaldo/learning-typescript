bde752df4d21e390a97189bb1042d96567cdb3d2
Prajwal Choudhary <prajwalchoudhary14@gmail.com>
2023-09-21 20:17:46 +0530

    learned about generics in TS

--- Lesson-1/build/js/main.js
+++ Lesson-1/build/js/main.js
@@ -1,53 +1,86 @@
 "use strict";
-// Index Signatures
-// interface TransactionObj {
-//     readonly [index: string]: number
-// }
-// interface TransactionObj {
-//     Pizza: number,
-//     Books: number,
-//     Job: number
-// }
-const todaysTransaction = {
-    Pizza: -10,
-    Books: -5,
-    Job: 50,
-    Prajwal: 23
+// This only works for string.
+const stringEcho = (arg) => arg;
+// This works for any type of argument
+const echo = (arg) => arg;
+// Most common usecase for generics is utility functions
+const isObj = (arg) => {
+    return (typeof arg === 'object' && !Array.isArray(arg) && arg != null);
 };
-console.log(todaysTransaction.Pizza);
-console.log(todaysTransaction['Pizza']);
-let prop = 'Pizza';
-console.log(todaysTransaction[prop]);
-const todaysNet = (transactions) => {
-    let sum = 0;
-    for (const transaction in transactions) {
-        sum += transactions[transaction];
+console.log(isObj(true));
+console.log(isObj('John'));
+console.log(isObj([1, 2, 3]));
+console.log(isObj({ name: 'Prajwal' }));
+console.log(isObj(null));
+const isTrue = (arg) => {
+    if (Array.isArray(arg) && !arg.length) {
+        return { arg, is: false };
     }
-    return sum;
+    if (isObj(arg) && !Object.keys(arg).length) {
+        return { arg, is: false };
+    }
+    return { arg, is: !!arg };
 };
-console.log(todaysNet(todaysTransaction));
-// won't work
-// todaysTransaction.Pizza = 32;
-// Well TS doesn't have an issue with this
-console.log(todaysTransaction['Prajwal']);
-const student = {
-    name: 'Prajwal',
-    GPA: 3.6,
-    classes: [123, 222]
+console.log(isTrue(false));
+console.log(isTrue(0));
+console.log(isTrue(true));
+console.log(isTrue(1));
+console.log(isTrue('Dave'));
+console.log(isTrue(''));
+console.log(isTrue(null));
+console.log(isTrue(undefined));
+console.log(isTrue({}));
+console.log(isTrue({ name: 'Prajwal' }));
+console.log(isTrue([]));
+console.log(isTrue([1, 2, 3]));
+console.log(isTrue(NaN));
+console.log(isTrue(-0));
+const checkBoolValue = (arg) => {
+    if (Array.isArray(arg) && !arg.length) {
+        return { value: arg, is: false };
+    }
+    if (isObj(arg) && !Object.keys(arg).length) {
+        return { value: arg, is: false };
+    }
+    return { value: arg, is: !!arg };
 };
-console.log(student.test);
-for (const key in student) {
-    console.log(`${key} : ${student[key]}`);
+// T extending the interface HasID narrows down the types
+// for T now T has to have an id property.
+const processUser = (user) => {
+    // process the user with logic
+    return user;
+};
+console.log(processUser({ id: 1, name: 'Dave' }));
+// Hover over the red squiggly line
+// console.log(processUser({ name: 'Prajwal' }));
+// the argument users is going to be an array of type T and each one of them must 
+// have an id property. And K extends the keys of T. So K is key of object T which 
+// necessarily have the property id as it implements the interface HasID.
+// T[K] is the value of key K of object T and T[K][] is an array of such values.
+const getUsersProperty = (users, key) => {
+    return users.map((user) => {
+        return user[key];
+    });
+};
+const usersProperty = getUsersProperty([{ name: 'John', id: 23 }, { name: 'Dave', id: 34 }], 'name');
+usersProperty.map((userProperty) => console.log(userProperty));
+class StateObject {
+    constructor(value) {
+        this.data = value;
+    }
+    get state() {
+        return this.data;
+    }
+    set state(value) {
+        this.data = value;
+    }
 }
-const logStudentKey = (student, key) => {
-    console.log(`Student ${key}: ${student[key]}`);
-};
-logStudentKey(student, 'GPA');
-const monthlyIncomes = {
-    salary: 100,
-    bonus: 50,
-    "side-hustle": 12
-};
-for (const revenue in monthlyIncomes) {
-    console.log(monthlyIncomes[revenue]);
-}
+const store = new StateObject('John');
+console.log(store.state);
+store.state = 'Dave';
+// This is an issue as passing 'John' makes TS to infer the type to string.
+// And now we can't assign numbers or any other type.
+//store.state = 12;
+const myState = new StateObject([13, 'Mikel']);
+myState.state = ['Dave', 69, 420];
+console.log(myState.state);
--- Lesson-1/src/main.ts
+++ Lesson-1/src/main.ts
@@ -1,96 +1,112 @@
-// Index Signatures
+// This only works for string.
+const stringEcho = (arg: string): string => arg;
 
-interface TransactionObj {
-    readonly [index: string]: number,
-    // Pizza, Books and Job are required
-    Pizza: number,
-    Books: number,
-    Job: number
+// This works for any type of argument
+const echo = <T>(arg: T): T => arg;
+
+// Most common usecase for generics is utility functions
+const isObj = <T>(arg: T): boolean => {
+    return (typeof arg === 'object' && !Array.isArray(arg) && arg != null) 
 }
 
-// interface TransactionObj {
-//     readonly [index: string]: number
-// }
+console.log(isObj(true));
+console.log(isObj('John'));
+console.log(isObj([1, 2, 3]));
+console.log(isObj({name: 'Prajwal'}));
+console.log(isObj(null));
 
-// interface TransactionObj {
-//     Pizza: number,
-//     Books: number,
-//     Job: number
-// }
-
-const todaysTransaction: TransactionObj = {
-    Pizza: -10,
-    Books: -5,
-    Job: 50,
-    Prajwal: 23
-}
-
-console.log(todaysTransaction.Pizza);
-console.log(todaysTransaction['Pizza']);
-
-let prop: string = 'Pizza';
-console.log(todaysTransaction[prop]);
-
-const todaysNet = (transactions: TransactionObj): number => {
-    let sum = 0;
-    for(const transaction in transactions) {
-        sum += transactions[transaction];
+const isTrue = <T>(arg: T): { arg: T, is: boolean } => {
+    if(Array.isArray(arg) && !arg.length) {
+        return {arg, is: false}
     }
-    return sum;
+    if(isObj(arg) && !Object.keys(arg as keyof T).length) {
+        return {arg, is: false}
+    }
+    return { arg, is: !!arg }
 }
 
-console.log(todaysNet(todaysTransaction));
+console.log(isTrue(false));
+console.log(isTrue(0));
+console.log(isTrue(true));
+console.log(isTrue(1));
+console.log(isTrue('Dave'));
+console.log(isTrue(''));
+console.log(isTrue(null));
+console.log(isTrue(undefined));
+console.log(isTrue({}));
+console.log(isTrue({name: 'Prajwal'}));
+console.log(isTrue([]));
+console.log(isTrue([1, 2, 3]));
+console.log(isTrue(NaN));
+console.log(isTrue(-0));
 
-// won't work
-// todaysTransaction.Pizza = 32;
-
-// Well TS doesn't have an issue with this
-console.log(todaysTransaction['Prajwal']);
-
-////////////////////////////////////////////////////////////
-
-interface Student {
-    [key: string]: (string | number | number[] | undefined),
-    name: string,
-    GPA: number,
-    classes?: number[],
+interface Boolcheck<T> {
+    value: T,
+    is: boolean
 }
 
-const student: Student = {
-    name: 'Prajwal',
-    GPA: 3.6,
-    classes: [123, 222]
-} 
-
-console.log(student.test);
-
-for(const key in student) {
-    console.log(`${key} : ${student[key]}`)
+const checkBoolValue = <T>(arg: T): Boolcheck<T> => {
+    if(Array.isArray(arg) && !arg.length) {
+        return {value: arg, is: false}
+    }
+    if(isObj(arg) && !Object.keys(arg as keyof T).length) {
+        return {value: arg, is: false}
+    }
+    return { value: arg, is: !!arg }
 }
 
-const logStudentKey = (student: Student, key: keyof Student): void => {
-    console.log(`Student ${key}: ${student[key]}`)
+interface HasID {
+    id: number
 }
 
-logStudentKey(student, 'GPA');
-
-///////////////////////////////////////////////////////////////
-
-// interface Income {
-//     [key: string]: number
-// }
-
-// Literal types can't be used as keys inside interface
-type Streams = 'salary' | 'bonus' | 'side-hustle'
-
-type Incomes = Record<Streams, number>
-
-const monthlyIncomes: Incomes = {
-    salary: 100,
-    bonus: 50,
-    "side-hustle": 12
+// T extending the interface HasID narrows down the types
+// for T now T has to have an id property.
+const processUser = <T extends HasID>(user: T): T => {
+    // process the user with logic
+    return user;
 }
 
-for(const revenue in monthlyIncomes) {
-    console.log(monthlyIncomes[revenue as keyof Incomes]);
+console.log(processUser({ id: 1, name: 'Dave'}));
+// Hover over the red squiggly line
+// console.log(processUser({ name: 'Prajwal' }));
+
+// the argument users is going to be an array of type T and each one of them must 
+// have an id property. And K extends the keys of T. So K is key of object T which 
+// necessarily have the property id as it implements the interface HasID.
+
+// T[K] is the value of key K of object T and T[K][] is an array of such values.
+const getUsersProperty = <T extends HasID, K extends keyof T>(users: T[], key: K): T[K][] => {
+    return users.map((user) => {
+        return user[key]
+    })
 }
+
+const usersProperty = getUsersProperty([{name:'John', id: 23}, {name: 'Dave', id: 34}], 'name');
+usersProperty.map((userProperty) => console.log(userProperty));
+
+class StateObject<T> {
+    private data: T
+    
+    constructor(value: T) {
+        this.data = value;
+    }
+
+    get state(): T {
+        return this.data;
+    }
+
+    set state(value: T) {
+        this.data = value;
+    }
+}
+
+const store = new StateObject('John')
+console.log(store.state);
+store.state = 'Dave';
+// This is an issue as passing 'John' makes TS to infer the type to string.
+// And now we can't assign numbers or any other type.
+//store.state = 12;
+
+const myState = new StateObject<(string | number | boolean)[]>([13, 'Mikel']);
+myState.state = ['Dave', 69, 420]
+console.log(myState.state);
